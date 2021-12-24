50f67b6c0a0278e9f735cfd1d278be875ab7c283
Emil Balicki <emil.balicki2@gmail.com>
2021-12-24 03:04:52 +0100

    Updated no shards scenario

--- deployments/noShards/main.tf
+++ deployments/noShards/main.tf
@@ -13,22 +13,34 @@ provider "google" {
   zone = "europe-west1-b"
 }
 
-resource "google_compute_instance" "mongodb" {
-  name = "imongo"
-  machine_type = "f1-micro"
+# Config Server for the MongoDB
+resource "google_compute_instance" "mongos" {
+  name = "mongos"
+  machine_type = "e2-standard-2"
   allow_stopping_for_update = true
-
+  # Specify the OS image
   boot_disk {
     initialize_params {
-      image = "cos-cloud/cos-93-16623-39-21"
+      image = "ubuntu-os-cloud/ubuntu-minimal-2004-focal-v20211209"
     }
    }
   
   network_interface {
     network = "default"
-
     access_config {}
   }
-
-  metadata_startup_script = "docker run --name mongodb -p 27017:27017 -d mongo:5.0.4"
+  # Upload the ssh public key
+  metadata = {
+    ssh-keys = "ubuntu:${file("./mongokey.pub")}"
+  }
+  # Start the script for 
+  provisioner "remote-exec" {
+    script = "scripts/mongosh.sh"
+    connection {
+      type = "ssh"
+      user = "ubuntu"
+      host = self.network_interface[0].access_config[0].nat_ip
+      private_key = file("./mongokey")
+    } 
+  }
 }

63d5749ff4df775c7aa13c9f2263b646bfbb62fd
Emil Balicki <emil.balicki2@gmail.com>
2022-02-03 16:01:22 +0100

    Fixed problems with logging

--- collect_results.py
+++ collect_results.py
@@ -1,10 +1,7 @@
-from posixpath import dirname
 import shutil
 import os
 from datetime import datetime
 import subprocess
-
-from pyrsistent import b
 
 # Prepare the name and the header
 with open("header.txt", "a") as header:
@@ -13,8 +10,8 @@ with open("header.txt", "a") as header:
     timeStr = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
     header.write("TimeOfBenchmark, "+ timeStr+"\n")
     with open("number_of_shards_in_last_run.txt", "r", encoding="utf-8") as f:
-        number_of_shards = f.read()
-        header.write("NumberOfShards,"+number_of_shards+"\n")
+        number_of_shards = int(f.read())
+        header.write(f"NumberOfShards,{number_of_shards}\n")
     # If first benchmark, write new file
     if not os.path.exists("id_of_last_run_benchmark.txt"):
         with open("id_of_last_run_benchmark.txt", "w") as f:
@@ -42,8 +39,8 @@ os.remove("deployments/benchmarking_client/clientIP.txt")
 # Append header
 fileNames = ["header.txt", "results.txt"]
 # Create the final result file
-dirName = f"Results{benchmark_id}_Shards{number_of_shards}_" + timeStr
-with open(f"{dirName}.txt", "wb") as finalFile:
+dirName = f"Results{benchmark_id}_Shards{number_of_shards}_" + timeStr + ".txt"
+with open(dirName, "wb") as finalFile:
     for f in fileNames:
         with open(f, 'rb') as fd:
             shutil.copyfileobj(fd,finalFile)
